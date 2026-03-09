3f41b1ef1b7f6ae16390b52883a3a8a0b3b2a1ed
stepik26 <max_aron@gmx.de>
2026-03-09 16:25:11 +0100

    sec

--- main.js
+++ main.js
@@ -5,58 +5,105 @@ let currentMonth = month;
 const year = today.getFullYear();
 let currentYear = year;
 
-const counterDay = ["erste", "zweite", "dritte", "vierte", "fünfte"];
+//----------------------------------------------------------------------------------------------------------------------//
 
-// Heutiges Datum ausgeschrieben
-const date = today.toLocaleDateString("de-DE", {
-  // locales, options
-  day: "numeric",
-  month: "long",
-  year: "numeric",
-});
+function dayClickEvents() {
+  const td = document.querySelectorAll("td");
+  for (let i = 0; i < td.length; i++) {
+    td[i].onclick = () => {
+      if (td[i].textContent !== "") {
+        //nur Tage mit Zahlen
+        for (el of td) {
+          el.classList.remove("selected"); //beim Klick auf einen anderen Tag wird die letzte Markierung entfernt
+        }
+        td[i].classList.add("selected");
+        const daySelected = td[i].textContent;
+        const selectedDate = new Date(currentYear, currentMonth, daySelected);
 
-let dateClass = document.querySelectorAll(".date");
-for (const el of dateClass) {
-  el.textContent = date;
+        renderInfo(selectedDate);
+      }
+    };
+  }
 }
 
-// Heutiger Wochentag ausgeschrieben
-let weekdayClass = document.querySelectorAll(".weekday");
-for (const el of weekdayClass) {
-  el.textContent = today.toLocaleDateString("de-DE", {
-    weekday: "long",
-  });
-}
+//----------------------------------------------------------------------------------------------------------------------//
 
-// der wievielte Wochentag
-let counter = document.querySelector("#counter");
-counter.textContent = counterDay[Math.ceil(day / 7) - 1];
+function renderInfo(dateObj) {
+  const day = dateObj.getDate();
+  const month = dateObj.getMonth();
+  const year = dateObj.getFullYear();
+  const counterDay = ["erste", "zweite", "dritte", "vierte", "fünfte"];
 
-let month1 = document.querySelectorAll(".month1");
-for (el of month1) {
-  el.textContent = today.toLocaleDateString("de-DE", {
+  // Heutiges Datum ausgeschrieben
+  const date = dateObj.toLocaleDateString("de-DE", {
+    // locales, options
+    day: "numeric",
     month: "long",
-  });
-}
-let year1 = document.querySelectorAll("#year1");
-for (el of year1) {
-  el.textContent = today.toLocaleDateString("de-DE", {
     year: "numeric",
   });
+
+  let dateClass = document.querySelectorAll(".date");
+  for (const el of dateClass) {
+    el.textContent = date;
+  }
+
+  // Heutiger Wochentag ausgeschrieben
+  let weekdayClass = document.querySelectorAll(".weekday");
+  for (const el of weekdayClass) {
+    el.textContent = dateObj.toLocaleDateString("de-DE", {
+      weekday: "long",
+    });
+  }
+
+  // der wievielte Wochentag
+  let counter = document.querySelector("#counter");
+  counter.textContent = counterDay[Math.ceil(day / 7) - 1];
+
+  let month1 = document.querySelectorAll(".month1");
+  for (el of month1) {
+    el.textContent = dateObj.toLocaleDateString("de-DE", {
+      month: "long",
+    });
+  }
+  let year1 = document.querySelectorAll("#year1");
+  for (el of year1) {
+    el.textContent = dateObj.toLocaleDateString("de-DE", {
+      year: "numeric",
+    });
+  }
+
+  // der wievielte Tag des Jahres
+  const startOfYear = new Date(year, 0, 1);
+  const diff = dateObj - startOfYear; // Differenz in Millisekunden
+  const oneDay = 24 * 60 * 60 * 1000;
+  const dayOfYear = Math.ceil(diff / oneDay) + 1; // + 1 damit 1.Januar = Tag 1 ist
+  document.querySelector("#dayOfYear").textContent = dayOfYear;
+
+  // wieviele Tage noch zum Jahresende
+  let daysInYear = (new Date(year + 1, 0, 1) - new Date(year, 0, 1)) / oneDay;
+  let daysLeft = daysInYear - dayOfYear + 1; //Starttag wird mitgezählt
+  document.querySelector("#daysLeft").textContent = daysLeft;
+
+  // wieviele Tage im Monat
+  document.querySelector("#daysInMonth").textContent = new Date(
+    year,
+    month + 1,
+    0,
+  ).getDate();
 }
-
-// der wievielte Tag des Jahres
-const startOfYear = new Date(year, 0, 1);
-const diff = today - startOfYear; // Differenz in Millisekunden
-const oneDay = 24 * 60 * 60 * 1000;
-const dayOfYear = Math.ceil(diff/oneDay);
-document.querySelector("#dayOfYear").textContent = dayOfYear;
-
+//----------------------------------------------------------------------------------------------------------------------//
 function renderMonth(currentYear, currentMonth) {
-  let startWeekDay = new Date(currentYear, currentMonth, 1).getDay(); // 0-6
-  startWeekDay = startWeekDay === 0 ? 6 : startWeekDay + 1; //jetzt startet eine Woche mit Montag
-  const daysInMonth = new Date(currentYear, currentMonth + 1, 0).getDate();
   const td = document.querySelectorAll("td");
+  // zuerst alles leeren
+  for (let i = 0; i < td.length; i++) {
+    td[i].textContent = "";
+    td[i].classList.remove("today", "weekend");
+  }
+
+  let startWeekDay = new Date(currentYear, currentMonth, 1).getDay(); // 0-6
+  startWeekDay = startWeekDay === 0 ? 6 : startWeekDay - 1; //jetzt startet eine Woche mit Montag
+  const daysInMonth = new Date(currentYear, currentMonth + 1, 0).getDate();
+
   for (let day = 0; day < startWeekDay; day++) {
     td[day].textContent = ""; // leere Tage vom Vormonat
   }
@@ -70,10 +117,14 @@ function renderMonth(currentYear, currentMonth) {
       currentMonth === today.getMonth() &&
       currentYear === today.getFullYear()
     ) {
-      day.style.backgroundColor = "yellow"; //heutigen Tag markieren
+      day.classList.add("today"); //heutigen Tag markieren
     } else if (index % 7 === 5 || index % 7 === 6) {
-      day.style.backgroundColor = "lightgray"; // Wochenende
+      day.classList.add("weekend"); // Wochenende
     }
+    // hide empty row
+    const trs = document.querySelectorAll("tr");
+    const lastTr = trs[trs.length - 1];
+    const tds = lastTr.querySelectorAll("td");
   }
 
   //calendar-header anpassen
@@ -90,6 +141,8 @@ function renderMonth(currentYear, currentMonth) {
   );
 }
 
+//----------------------------------------------------------------------------------------------------------------------//
+
 function renderCalendar() {
   renderMonth(currentYear, currentMonth);
 
@@ -102,6 +155,10 @@ function renderCalendar() {
       currentYear--;
     }
     renderMonth(currentYear, currentMonth);
+    const td = document.querySelectorAll("td");
+    for (el of td) {
+          el.classList.remove("selected"); //die letzte Markierung entfernt
+        }
   };
 
   nextMonth = document.querySelector("#nextMonth");
@@ -112,7 +169,13 @@ function renderCalendar() {
       currentYear++;
     }
     renderMonth(currentYear, currentMonth);
+    const td = document.querySelectorAll("td");
+    for (el of td) {
+          el.classList.remove("selected"); //die letzte Markierung entfernt
+        }
   };
+  dayClickEvents();
+  //----------------------------------------------------------------------------------------------------------------------//
 }
-
+renderInfo(new Date());
 renderCalendar();
